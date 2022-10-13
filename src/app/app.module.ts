5ef9b95a2fc335124d26930e3175f698ab10b2aa
108432TorresMauricio <108432@tecnicatura.frc.utn.edu.ar>
2022-10-12 21:30:29 -0300

    actualizacion de modal
    agregue modal al navbar y borre componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { ServiciosOfrecidosComponent } from './components/servicios-ofrecidos/servicios-ofrecidos.component';
-import { ModalComponent } from './components/modal/modal.component';
+
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, BannerComponent, ServiciosOfrecidosComponent, ModalComponent],
+  declarations: [AppComponent, NavbarComponent, BannerComponent, ServiciosOfrecidosComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
