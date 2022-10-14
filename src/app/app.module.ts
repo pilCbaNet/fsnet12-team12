4fbda75538f65e122d4d98dceb905ad9f89ed81d
Alejandro Bazan <elalebazan@gmail.com>
2022-10-14 18:25:23 -0300

    Footer completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 //Componentes
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { ServiciosOfrecidosComponent } from './components/servicios-ofrecidos/servicios-ofrecidos.component';
 import { FormComponent } from './components/form/form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, BannerComponent, ServiciosOfrecidosComponent, FormComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    ServiciosOfrecidosComponent,
+    FormComponent,
+    FooterComponent,
+  ],
 
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
