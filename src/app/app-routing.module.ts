86287ba5bc2595b842ec01d4eafd04854deed3ea
DanielaCrapa <83256109+DanielaCrapa@users.noreply.github.com>
2022-10-14 23:11:13 -0300

    ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { MovimientosComponent } from './components/movimientos/movimientos.component';
+import { OperacionesComponent } from './components/operaciones/operaciones.component';
+import { ServiciosOfrecidosComponent } from './components/servicios-ofrecidos/servicios-ofrecidos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:ServiciosOfrecidosComponent},
+  {path:'menu', component:MenuComponent,
+children:[
+  {path: 'movimientos', component:MovimientosComponent},
+  {path: 'operaciones', component:OperacionesComponent},
+]},
+  {path: '', redirectTo:'/home', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ServiciosOfrecidosComponent } from './components/servicios-ofrecidos/se
 import { FormComponent } from './components/form/form.component';
 import { FooterComponent } from './components/Layout/footer/footer.component';
 import { OperacionesComponent } from './components/operaciones/operaciones.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MovimientosComponent } from './components/movimientos/movimientos.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,8 @@ import { OperacionesComponent } from './components/operaciones/operaciones.compo
     FormComponent,
     FooterComponent,
     OperacionesComponent,
+    MenuComponent,
+    MovimientosComponent,
   ],
 
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
