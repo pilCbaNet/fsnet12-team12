8621d228077a0df6ae47e305d591ec801beed430
108432TorresMauricio <108432@tecnicatura.frc.utn.edu.ar>
2022-10-19 17:01:54 -0300

    inicio session

---
+++ src/app/servicios/inicio-session/inicio-session.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Login } from 'app/models/login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InicioSessionService {
+
+  constructor(private http:HttpClient) { }
+
+
+  iniciar_session(login:Login):Observable<any>
+  {
+    return this.http.post('',login)
+  }
+
+}
