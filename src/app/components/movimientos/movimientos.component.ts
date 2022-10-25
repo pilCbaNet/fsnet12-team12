1edf795666147e130677698e9f66145c0e5a511f
Franco Gual <ignacio.gual@mi.unc.edu.ar>
2022-10-24 22:22:03 -0300

    Limpiando el codigo. Algunos cambios de estilo.

--- src/app/components/movimientos/movimientos.component.ts
+++ src/app/components/movimientos/movimientos.component.ts
@@ -8,36 +8,6 @@ import { MovimientosService } from 'app/servicios/movimientos/movimientos.servic
   styleUrls: ['./movimientos.component.css'],
 })
 export class MovimientosComponent implements OnInit {
-  /*
-  movimientos: any = [
-    {
-      id: 1,
-      date: '15/01/2022',
-      mount: 1000,
-      description: 'Deposit cash',
-      accountNumber: '1234567890',
-      accountName: 'Fernando Yañez',
-      type: 'Deposit',
-    },
-    {
-      id: 2,
-      date: '15/02/2022',
-      mount: 2000,
-      description: 'Transfer cash',
-      accountNumber: '1234567891',
-      accountName: 'Hortensia Marrero',
-      type: 'Withdraw',
-    },
-    {
-      id: 3,
-      date: '15/03/2022',
-      mount: 3000,
-      description: 'Deposit cash',
-      accountNumber: '1234567892',
-      accountName: 'Iratxe Sancho',
-      type: 'Deposit',
-    },
-  ];*/
   movimientos: any = [];
   reset: any;
   now: any = new Date();
@@ -64,11 +34,6 @@ export class MovimientosComponent implements OnInit {
       this.arrayExpanded = this.reset.map((_: any) => false);
       console.log(this.movimientos);
     });
-
-    /*
-    this.reset = this.movimientos;
-    this.arrayExpanded = this.reset.map((_: any) => false);
-*/
   }
 
   sortList(): void {
@@ -79,7 +44,6 @@ export class MovimientosComponent implements OnInit {
     if (filterByType != '') {
       console.log('this is the filtered type: ' + filterByType);
       newFilteredList = newFilteredList.filter((item: any) => {
-        //return item.type == filterByType;
         return item.description.split(' ')[0] == filterByType;
       });
     }
