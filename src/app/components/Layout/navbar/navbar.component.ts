72b2f0b62cb6c3c65fccc9605888ae1148662318
Alejandro Bazan <elalebazan@gmail.com>
2022-10-31 18:55:42 -0300

    inicio sesion

--- src/app/components/Layout/navbar/navbar.component.ts
+++ src/app/components/Layout/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder,Validators } from '@angular/forms'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Login } from 'app/models/login';
 import { InicioSessionService } from 'app/servicios/inicio-session/inicio-session.service';
@@ -7,41 +7,35 @@ import { InicioSessionService } from 'app/servicios/inicio-session/inicio-sessio
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
-  form!:FormGroup;
-  constructor(private formBuilder:FormBuilder , private miServicioInicioSession:InicioSessionService, private router:Router) { 
-    this.form=this.formBuilder.group({
-      email:['', [Validators.required, Validators.email]],
-      pass:['', [Validators.required]]
-    })
-
+  form!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private miServicioInicioSession: InicioSessionService,
+    private router: Router
+  ) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      pass: ['', [Validators.required]],
+    });
   }
-  
-  ngOnInit(): void {
-  }
 
-  login()
-  {
-    if(this.form.valid) 
-    {
-      let email:string= this.form.get("email")?.value;
-      let password:string= this.form.get("pass")?.value;
-      let login:Login= new Login(email,password);
-      this.miServicioInicioSession.iniciar_session(login).subscribe(data => { 
-        document.getElementById("modal-cerrar")?.click();
-        alert("redirigiendo...")
-        this.router.navigate(["form"])
+  ngOnInit(): void {}
+
+  login() {
+    if (this.form.valid) {
+      let email: string = this.form.get('email')?.value;
+      let password: string = this.form.get('pass')?.value;
+      let login: Login = new Login(email, password);
+      this.miServicioInicioSession.iniciar_session(login).subscribe((data) => {
+        document.getElementById('modal-cerrar')?.click();
+        alert('redirigiendo...');
+        this.router.navigate(['dashboard']);
       });
-      
+    } else {
+      alert('INGRESE UN CAMPO VÁLIDO');
     }
-    else
-    {
-      alert("INGRESE UN CAMPO VÁLIDO");
-    }
-
-  };
-
+  }
 }
