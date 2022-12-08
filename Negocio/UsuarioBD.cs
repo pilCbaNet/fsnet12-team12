25fd89306c938147b34322ffc48f3d7a6452f37c
108432TorresMauricio <108432@tecnicatura.frc.utn.edu.ar>
2022-12-08 20:01:05 -0300

    usuarioBD

---
+++ Negocio/UsuarioBD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Entities;
+using System.Collections.Generic;
+using Linq;
+using Text;
+using System.Threading.Tasks;
+
+public class UsuarioBD
+{
+	public UsuarioBD()
+	{
+		public Usuarios login(MiBilleteraContext bd, string Email, string Contraseña)
+		{
+			return (Usuarios?) bd.Usuarios.FirstOrDefault(a => a.Email == Email && a.Contraseña == Contraseña)
+		}
+	}
+}
