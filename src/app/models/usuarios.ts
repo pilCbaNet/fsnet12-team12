ad4ccd5ea9746356cde3fa61c289800c85304ff9
Alejandro Bazan <elalebazan@gmail.com>
2022-12-10 16:09:05 +0100

    models usuario.ts y quienes somos

--- src/app/models/usuarios.ts
+++ src/app/models/usuarios.ts
@@ -1,40 +1,31 @@
 export class Usuarios {
   nombre: string;
-  apellido: string;
   fechaNacimiento: Date;
   email: string;
   usuario1: string;
-  constraseña: string;
+  contraseña: string;
   telefono: string;
-  fechaAlta: Date;
-  fechaBaja: any;
   domicilio: string;
   dni: string;
   idCuenta: number;
 
   constructor(
     nombre: string,
-    apellido: string,
     fechaNacimiento: Date,
     email: string,
     usuario1: string,
-    constraseña: string,
+    contraseña: string,
     telefono: string,
-    fechaAlta: Date,
-    fechaBaja: any,
     domicilio: string,
     dni: string,
     idCuenta: number
   ) {
     this.nombre = nombre;
-    this.apellido = apellido;
     this.fechaNacimiento = fechaNacimiento;
     this.email = email;
     this.usuario1 = usuario1;
-    this.constraseña = constraseña;
+    this.contraseña = contraseña;
     this.telefono = telefono;
-    this.fechaAlta = fechaAlta;
-    this.fechaBaja = fechaBaja;
     this.domicilio = domicilio;
     this.dni = dni;
     this.idCuenta = idCuenta;
