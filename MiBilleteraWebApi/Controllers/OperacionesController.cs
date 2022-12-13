25dc6401a8545c596b83242a7fb73b3df3083f87
Franco Gual <ignacio.gual@mi.unc.edu.ar>
2022-12-13 11:03:39 -0300

    Post Deposito.

--- MiBilleteraWebApi/Controllers/OperacionesController.cs
+++ MiBilleteraWebApi/Controllers/OperacionesController.cs
@@ -65,8 +65,43 @@ namespace MiBilleteraWebApi.Controllers
             }
         }
 
-            // PUT api/<OperacionesController>/5
-            [HttpPut("{id}")]
+        // POST api/<OperacionesController>/Deposito
+        [HttpPost("Deposito")]
+        public void Post([FromBody] VistaDeposito vistaDeposito)
+        {
+            using (var db = new MiBilleteraContext())
+            {
+                TipoOperacion result = db.TipoOperacions.FirstOrDefault(a => a.Descripcion == "Deposito en efectivo");
+                Usuarios userResult = db.Usuarios.FirstOrDefault(a => a.IdUsuario == vistaDeposito.idUsuario);
+
+                if (result == null)
+                {
+                    TipoOperacion nuevaOperacion = new TipoOperacion();
+                    nuevaOperacion.Descripcion = "Deposito en efectivo";
+                    db.TipoOperacions.Add(nuevaOperacion);
+                    db.SaveChanges();
+                    result = db.TipoOperacions.FirstOrDefault(a => a.Descripcion == "Deposito en efectivo");
+                }
+                Operaciones oOperacion = new Operaciones();
+
+                oOperacion.Monto = vistaDeposito.Monto;
+                oOperacion.Fecha = DateTime.Now;
+                oOperacion.DniRetiro = vistaDeposito.DniRetiro;
+                oOperacion.IdUsuario = vistaDeposito.idUsuario;
+                oOperacion.IdTipoOperacion = result.IdTipoOperacion;
+                oOperacion.IdTipoOperacionNavigation = result;
+                oOperacion.IdUsuarioNavigation = userResult;
+
+                Cuentas accountResult = db.Cuentas.FirstOrDefault(a => a.IdCuenta == userResult.IdCuenta);
+                accountResult.Saldo += decimal.ToInt32(vistaDeposito.Monto);
+
+                db.Operaciones.Add(oOperacion);
+                db.SaveChanges();
+            }
+        }
+
+        // PUT api/<OperacionesController>/5
+        [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
